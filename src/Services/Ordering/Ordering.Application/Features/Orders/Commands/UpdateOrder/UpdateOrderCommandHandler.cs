7ab76929cb3717cde05aeffdb0d5833f2c4060ab
wailinnaung-wla7 <wailinnaung.wla7@gmail.com>
2022-09-23 19:44:51 +0630

    add delete handler

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
 using Ordering.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -31,7 +32,7 @@ namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
             if (orderToUpdate == null)
             {
                 _logger.LogError("Order Not Exist on database.");
-                //throw new NotFoundException(nameof(Order), request.Id);
+                throw new NotFoundException(nameof(Order), request.Id);
             }
             _mapper.Map(request, orderToUpdate);
             //_mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
