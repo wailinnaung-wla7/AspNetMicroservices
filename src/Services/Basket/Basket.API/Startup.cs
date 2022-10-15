772745b6ab37a49b5146ef54f48770d94295f947
wailinnaung-wla7 <wailinnaung.wla7@gmail.com>
2022-10-15 23:23:25 +0630

    add building blocks for masstransit as class library add configuration of rabbitmq in basket.api

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -34,8 +35,17 @@ namespace Basket.API
             });
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
-                (o=> o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+                (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
             services.AddScoped<DiscountGrpcService>();
+
+            services.AddMassTransit(config =>
+            {
+                config.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
